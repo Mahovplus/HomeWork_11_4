95101ba27937ea7ca15a14e63ae3034ae5e6f115
Даниил <mahovplus66@mail.ru>
2024-12-16 19:57:34 +0300

    Update module_11_4.py

--- module_11_4.py
+++ module_11_4.py
@@ -8,6 +8,7 @@ def introspection_info(other):
         dict_other.setdefault(str(tools.__name__), result)
     dict_other['module'] = introspection_info.__module__
     dict_other['attributes'] = [x for x in dir(other) if not callable(getattr(other, x))]
+    dict_other['metodes'] = [x for x in dir(other) if  callable(getattr(other, x))]
     return dict_other
 
 def_call = introspection_info(42)
